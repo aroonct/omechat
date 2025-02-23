d71e77893d2d298824939502a9f7b2136afc8711
Aroon <84980760+aroonct@users.noreply.github.com>
2025-02-23 00:07:58 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const socket = new WebSocket("ws://localhost:3000");
+const socket = new WebSocket("wss://omechat-alpha.vercel.app");
 
 const localVideo = document.getElementById("localVideo");
 const remoteVideo = document.getElementById("remoteVideo");
